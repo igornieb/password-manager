e1ab49007ae786cdc33a4c2afcc35800c5550181
igornieb <igornieb@gmail.com>
2023-01-28 18:06:08 +0100

    added encryption and decryption methods

--- entry.py
+++ entry.py
@@ -1,5 +1,8 @@
 import sqlite3
 from account import Account
+from pbkdf2 import PBKDF2
+from Crypto.Cipher import AES
+from base64 import b64encode, b64decode
 
 
 class Entry:
@@ -9,18 +12,43 @@ class Entry:
         self.website = website
         self.password = password
 
-    def add(self, user: Account, website: str, plain_password: str):
-        # add new entry for given user object
+    def add(self):
         try:
+            hashed_password = self.encrypt()
             db_conn = sqlite3.connect('password-manager.sqlite')
             c = db_conn.cursor()
-            db_query = c.execute(
-                '''INSERT INTO `entries` ('owner','username','salt','password') VALUES ("{self.owner.username}", "{self.username}", "{self.website}", "{self.password}")''')
-            db_query.execute()
+            db_query = c.execute(f'''INSERT INTO `entries` ('owner','username','website','password') VALUES ("{self.owner.username}", "{self.username}", "{self.website}", "{hashed_password}")''')
+            db_conn.commit()
             db_conn.close()
             return True
         except:
             return False
 
-    def __str__(self):
-        return f"{self.website} {self.username}"
+    def encrypt(self):
+        message = self.password
+        key = PBKDF2(str(self.owner.hashed_password), self.owner.salt).read(32)
+        data_convert = str.encode(message)
+        cipher = AES.new(key, AES.MODE_EAX)
+        nonce = cipher.nonce
+        ciphertext, tag = cipher.encrypt_and_digest(data_convert)
+        add_nonce = ciphertext + nonce
+        encoded_ciphertext = b64encode(add_nonce).decode()
+        return encoded_ciphertext
+
+    def decrypt(self):
+        message = self.password
+        if len(message) % 4:
+            message += '=' * (4 - len(message) % 4)
+        convert = b64decode(message)
+        key = PBKDF2(str(self.owner.hashed_password), self.owner.salt).read(32)
+        nonce = convert[-16:]
+        cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+        plaintext = cipher.decrypt(convert[:-16])
+
+        return plaintext.decode()
+
+    # def __str__(self):
+    #     return f"{self.username} {self.website} {self.password}"
+
+    def __repr__(self):
+        return f"{self.username} {self.website} {self.decrypt()}"
