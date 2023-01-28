84c27a715561c134a27d8f09b8446cb0d2383468
igornieb <igornieb@gmail.com>
2023-01-28 18:05:48 +0100

    fixed login, register

--- account.py
+++ account.py
@@ -1,6 +1,7 @@
 import hashlib
 import sqlite3
 import secrets
+import string
 
 
 class Account:
@@ -20,29 +21,33 @@ class Account:
     def login(self):
         db_conn = sqlite3.connect('password-manager.sqlite')
         c = db_conn.cursor()
-        db_pass = c.execute(f"SELECT 'password' FROM `accounts` WHERE username='{self.username}'")
-        db_conn.close()
-        if db_pass == self.hashed_password:
-            self.salt = c.execute(f"SELECT 'salt' FROM `accounts` WHERE username='{self.username}'")
+        db_password=c.execute(f"SELECT password FROM `accounts` WHERE username='{self.username}'")
+        db_password=db_password.fetchone()[0]
+        if db_password == self.hashed_password:
+            self.salt = c.execute(f"SELECT salt FROM `accounts` WHERE username='{self.username}'").fetchone()[0]
             self.authenticated = True
-            return True
-        else:
-            return False
-
-    def register(self):
-        db_conn = sqlite3.connect('password-manager.sqlite')
-        c = db_conn.cursor()
-        db_username = c.execute(f"SELECT 'username' FROM `accounts` WHERE username='{self.username}'")
-        if db_username != self.username:
-            self.salt = str(secrets.SystemRandom())
-            c.execute(
-                f'''INSERT INTO `accounts`('username','salt','password') VALUES ("{self.username}", "{self.salt}", "{self.hashed_password}")''')
-            db_conn.commit()
             db_conn.close()
             return True
         else:
+            db_conn.close()
+            return False
+
+    def register(self):
+        try:
+            db_conn = sqlite3.connect('password-manager.sqlite')
+            c = db_conn.cursor()
+            alphabet = string.ascii_letters + string.digits
+            self.salt = ''.join(secrets.choice(alphabet) for i in range(100))
+            c.execute(f'''INSERT INTO `accounts`('username','salt','password') VALUES ("{self.username}", "{self.salt}", "{self.hashed_password}")''')
+            db_conn.commit()
+            db_conn.close()
+            return True
+        except:
             return False
 
     # TODO change_password, delete_account
     def __str__(self):
         return self.username
+
+    def __repr__(self):
+        return f"{self.username}"
